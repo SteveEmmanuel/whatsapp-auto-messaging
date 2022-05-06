6f9e3893fe8c9905535333e219f5a6e9286288ba
Steve Emmanuel <steve.emmanuelck@gmail.com>
2022-05-06 10:56:17 +0530

    read contacts from csv

--- run.py
+++ run.py
@@ -4,6 +4,7 @@ from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException, TimeoutException, StaleElementReferenceException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
+import csv
 
 
 def load_chat_with_phone_number(driver, phone_number):
@@ -50,37 +51,48 @@ if __name__ == '__main__':
                                       options=options)  # Change the path as per your local dir.
     chrome_browser.get('https://web.whatsapp.com/')
 
-    user_list = [('Kiran Benny', 919497896927), ('steve jio', 917012983478), ('stevsse jio', 919497896927)]
-
-    for user in user_list:
-        print('User loop entered')
-        user_element = check_presence_of_element_with_css_selector(chrome_browser, 'span[title="{}"]'.format(user[0]))
-        print('User presence checked')
-        if user_element is not None:
-            print('User present, trying to click')
-            try:
-                user_element.click()
-            except StaleElementReferenceException as se:
-                print('Stale element found, will find the user element again.')
+    with open('contacts.csv') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        line_count = 0
+        for user_row in csv_reader:
+            if line_count == 0:
+                print(f'Column names are {", ".join(user_row)}')
+                line_count += 1
+            else:
+                print(f'User by the name {user_row[0]} with phone number {user_row[1]}.')
+                line_count += 1
+                print('User loop entered')
                 user_element = check_presence_of_element_with_css_selector(chrome_browser,
-                                                                           'span[title="{}"]'.format(user[0]))
-                try:
-                    user_element.click()
-                except StaleElementReferenceException as se:
-                    break
-        else:
-            # No user by the specified name in chat history
-            # So looking in create new chat
-            print('User not present')
-            load_chat_with_phone_number(chrome_browser, user[1])
+                                                                           'span[title="{}"]'.format(user_row[0]))
+                print('User presence checked')
+                if user_element is not None:
+                    print('User present, trying to click')
+                    try:
+                        user_element.click()
+                    except StaleElementReferenceException as se:
+                        print('Stale element found, will find the user element again.')
+                        user_element = check_presence_of_element_with_css_selector(chrome_browser,
+                                                                                   'span[title="{}"]'.format(
+                                                                                       user_row[0]))
+                        try:
+                            user_element.click()
+                        except StaleElementReferenceException as se:
+                            break
+                else:
+                    # No user by the specified name in chat history
+                    # So trying to create new chat
+                    print('User not present')
+                    load_chat_with_phone_number(chrome_browser, user_row[1])
 
-        message_box = check_presence_of_element_with_css_selector(chrome_browser, '._2vJ01 ._3FRCZ')
-        if message_box is not None:
-            message_box.send_keys('hey')
+                message_box = check_presence_of_element_with_css_selector(chrome_browser, '._2vJ01 ._3FRCZ')
+                if message_box is not None:
+                    message_box.send_keys('hey')
 
-            send_button = check_presence_of_element_with_css_selector(chrome_browser, '._1U1xa')
-            # Click on send button
-            if send_button is not None:
-                send_button.click()
-        print('User loop exited')
+                    send_button = check_presence_of_element_with_css_selector(chrome_browser, '._1U1xa')
+                    # Click on send button
+                    if send_button is not None:
+                        send_button.click()
+                print('User loop exited')
+
+    print(f'Processed {line_count} lines. \n')
     chrome_browser.close()
